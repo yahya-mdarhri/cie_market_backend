766fcfdfb77d031c69073f8d3e9980289c24c7fc
hex01e <houmanso@student.1337.ma>
2025-04-10 16:43:37 +0100

    chore: setup env variables with decouple
    install decouple and setup Config for env variables
    update .giteignore and requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import Config
+
+config = Config('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$#wl1u_$wj^y6%%rgg7ye@yf30tbertnbs)4&ra^zf)g(j9p2m'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
