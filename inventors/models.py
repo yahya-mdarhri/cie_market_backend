007732d214eef91ab2720feb5eb90b3f2a53eb7d
hex01e <houmanso@student.1337.ma>
2025-04-11 15:35:13 +0100

    chore: Refactor Inventor model & add name_variants
    - set fields details depending on the given sql dump
    - adding name_varrieants as an array of CharField
    - make inventor just a smaple model

--- inventors/models.py
+++ inventors/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 
-class Inventor(AbstractUser):
-  preferred_name = models.CharField(max_length=255, blank=True, null=True)
-  # name variants : adding this field later when its datatype is decided
+class Inventor(models.Model):
+  Inventor_id = models.CharField(max_length=10, unique=True, primary_key=True) # 10 characters is really limited since 'inv-'+number , so 1000000 fileds are possible
+  preferred_name = models.CharField(max_length=150, null=False)
+  name_variants = models.ArrayField(
+    models.CharField(max_length=30),
+    blank=True,
+    null=True,
+    default=list,
+  )
   # affiliation : adding affiliation field later when its model is ready
   email = models.EmailField(unique=True)
-  image = models.ImageField(upload_to='inventor_images/', blank=True, null=True)
-  orcid = models.UUIDField(unique=True, blank=True, null=True)
-  phone_number = models.CharField(max_length=20, blank=True, null=True)
+  image = models.ImageField(upload_to='inventors/images/', default=None, blank=True, null=True)
+  orcid = models.CharField(max_length=19, unique=True, default=None, blank=True, null=True)
+  phone_number = models.CharField(max_length=11, blank=True, null=True, default=None)
 
   def __str__(self):
     return self.preferred_name
