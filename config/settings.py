33c2fac8dfd55de201f93c64e10f7b7d9c3deae7
hex01e <houmanso@student.1337.ma>
2025-04-13 15:02:55 +0100

    chore: switching to postgres

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from decouple import Config, Csv
 
 config = Config('.env')
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
