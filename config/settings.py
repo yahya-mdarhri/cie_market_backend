1f6e4493bbcfe39f04a8cd37aa53c304bd2f4ecb
hex01e <houmanso@student.1337.ma>
2025-06-03 13:44:03 +0100

    refactor: remove and reconfig Config load

--- config/settings.py
+++ config/settings.py
@@ -14,11 +14,10 @@ from datetime import timedelta
 from pathlib import Path
 from decouple import Config, Csv, RepositoryEnv
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env_path = BASE_DIR / '.env'
 config = Config(RepositoryEnv(str(env_path)))
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
