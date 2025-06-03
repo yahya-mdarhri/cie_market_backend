47855b3321379ad53ceeb590136e2607109f0e9d
hex01e <houmanso@student.1337.ma>
2025-06-03 13:41:14 +0100

    fix: fix loading env variables

--- config/settings.py
+++ config/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
-from decouple import Config, Csv
+from decouple import Config, Csv, RepositoryEnv
 
-config = Config('.env')
+BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = BASE_DIR / '.env'
+config = Config(RepositoryEnv(str(env_path)))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
