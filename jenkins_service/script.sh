99ea629430bc5003dc83d832cc3965e0a9f73583
hossine eddahbi <eddahbi.hossine.solicode@gmail.com>
2025-06-12 20:31:54 +0100

    update jenkins's docker file and the script

---
+++ jenkins_service/script.sh
@@ -0,0 +1,8 @@
+docker run -d \
+  --name jenkins-server \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -v jenkins_home:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --group-add 999 \ 
+  jenkins-docker
