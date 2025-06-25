e6625f781c5f6ed3b311e662ec27ffa44fb0d789
EH. Eddahbi <92024331+eddahbihossine@users.noreply.github.com>
2025-06-25 11:14:21 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 python -m venv env
 source env/bin/activate
 python -m pip install --upgrade pip
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 python manage.py migrate
 python manage.py runserver
