1871c063b058812468ed9900e019ecc7ca217b87
hossine eddahbi <eddahbi.hossine.solicode@gmail.com>
2025-06-25 11:28:25 +0100

    add vercel.json

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 python -m venv env
 source env/bin/activate
 python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+pip install -r requirements.txt
 python manage.py migrate
 python manage.py runserver
