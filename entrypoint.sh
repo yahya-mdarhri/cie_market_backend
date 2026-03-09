52440b510dca150e4d861787f79dc45576aa8af0
yahya mdarhri <78668874+yahya-mdarhri@users.noreply.github.com>
2026-03-09 14:48:26 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 set -e
-
 python manage.py migrate --noinput
-
 if [ "${DJANGO_COLLECTSTATIC:-1}" = "1" ]; then
   python manage.py collectstatic --noinput
 fi
-
-exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+exec gunicorn config.wsgi:application \
+  --bind 0.0.0.0:${PORT:-8000} \
+  --workers ${GUNICORN_WORKERS:-3} \
+  --timeout ${GUNICORN_TIMEOUT:-120} \
+  --keep-alive 5 \
+  --log-level info
